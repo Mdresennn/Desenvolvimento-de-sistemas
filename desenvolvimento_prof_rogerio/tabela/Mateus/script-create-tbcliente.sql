2125ff34ffe301f27b50fe972cf71c701242cc46
Mateus Wandresen Cardoso <mateus_w_cardoso@estudante.sesisenai.org.br>
2025-03-12 17:24:29 -0300

    Add files via upload

---
+++ desenvolvimento_prof_rogerio/tabela/Mateus/script-create-tbcliente.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_cliente(id,nm_cliente,ds_endereco,nr_telefone,ds_email)
+VALUE(1,"Felipe","End Felipe",479999999,"felipe@gmail.com");
+
+INSERT INTO tb_cliente(id,nm_cliente,ds_endereco,nr_telefone,ds_email)
+VALUE(2,"Rogerio","End Rogerio",4788899,"rogerio@gmail.com");
+
+INSERT INTO tb_cliente(id,nm_cliente,ds_endereco,nr_telefone,ds_email)
+VALUE(3,"Bruno","End Bruno",47777777799,"bruno@gmail.com");
